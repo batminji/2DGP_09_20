12ce3f7f0d10ad63c57bf9b97eb17389bc20cdbd
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-09-20 18:01:00 +0900

    원 함수 오류 수정

--- 09-20.py
+++ 09-20.py
@@ -4,18 +4,18 @@ from pico2d import *
 
 open_canvas()
 
-grass = load_image('grass_png')
+grass = load_image('grass.png')
 character = load_image('character.png')
 
 def run_circle():
-    cx, cy, r = 600, 300, 200
+    cx, cy, r = 400, 300, 200
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(deg / 360 * 2 * math.pi)
         y = cy + r * math.sin(deg / 360 * 2 * math.pi)
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        delat(0.01)
+        delay(0.01)
         
 def run_rectangle():
     print('REACTANGLE')
