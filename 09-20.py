59791aac9a292c815b1becb792a76477227a4786
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-09-20 17:59:49 +0900

    원 운동 함수 추가

--- 09-20.py
+++ 09-20.py
@@ -8,16 +8,15 @@ grass = load_image('grass_png')
 character = load_image('character.png')
 
 def run_circle():
-    print('CIRCLE')
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(400,90)
-    delay(1)
-
-    
-
-    pass
-
+    cx, cy, r = 600, 300, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delat(0.01)
+        
 def run_rectangle():
     print('REACTANGLE')
     pass
